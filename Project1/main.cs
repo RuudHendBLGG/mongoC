bc045d4098135acb8bd0b3a508fba70339bcf241
rguikers <jibespwd>
2014-08-04 15:40:56 +0200

    First commit

---
+++ Project1/main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Project1
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://mongodb.test.local:27017");
+            Sample test = new Sample { name = "Rob", description = "de beste" };
+            MongoDatabase database = client.GetServer().GetDatabase("blgg");
+            database.GetCollection("samples").Insert(test);
+        }
+    }
+}
